a6cc05905e0e345bc8be3a51ac7953b5f07be0e5
Dim <dmitrijzukov@eaton.com>
2021-05-31 21:05:37 +0200

    Try to fix test

--- tests/sensor_gpio_server.cpp
+++ tests/sensor_gpio_server.cpp
@@ -10,8 +10,57 @@
 extern zmsg_t* hw_cap_test_reply_gpi;
 extern zmsg_t* hw_cap_test_reply_gpo;
 
+void libgpio_test()
+{
+    const char* SELFTEST_DIR_RW = ".";
+
+    // Note: for testing purpose, we use a trick, that is to access the /sys
+    // FS under our SELFTEST_DIR_RW.
+
+    //  @selftest
+    //  Simple create/destroy test
+    libgpio_t* self = libgpio_new();
+    REQUIRE(self);
+    libgpio_destroy(&self);
+
+    // Setup
+    self = libgpio_new();
+    REQUIRE(self);
+    libgpio_set_test_mode(self, true);
+    libgpio_set_gpio_base_address(self, 0);
+    // We use the same offset for GPI and GPO, to be able to write a GPO
+    // and read the same for GPI
+    libgpio_set_gpi_offset(self, 0);
+    libgpio_set_gpo_offset(self, 0);
+    libgpio_set_gpi_count(self, 10);
+    libgpio_set_gpo_count(self, 5);
+
+    // Write test
+    // Let's first write to the dummy GPIO, so that the read works afterward
+    CHECK(libgpio_write(self, 1, GPIO_STATE_CLOSED) == 0);
+
+    // Read test
+    CHECK(libgpio_read(self, 1, GPIO_DIRECTION_IN) == GPIO_STATE_CLOSED);
+
+    // Value resolution test
+    CHECK(libgpio_get_status_value("opened") == GPIO_STATE_OPENED);
+    CHECK(libgpio_get_status_value("closed") == GPIO_STATE_CLOSED);
+    CHECK(libgpio_get_status_value(libgpio_get_status_string(GPIO_STATE_CLOSED).c_str()) == GPIO_STATE_CLOSED);
+
+    // Delete all test files
+    std::string sys_fn = std::string(SELFTEST_DIR_RW) + "/sys";
+    zdir_t*     dir    = zdir_new(sys_fn.c_str(), NULL);
+    CHECK(dir);
+    zdir_remove(dir, true);
+    zdir_destroy(&dir);
+
+    libgpio_destroy(&self);
+}
+
+
 TEST_CASE("sensor gpio server test")
 {
+    libgpio_test();
     // FIXME: disable -server test for now, while waiting to catch
     // the malamute race-cond leak
     // See https://github.com/42ity/fty-sensor-gpio/issues/11
@@ -38,10 +87,8 @@ TEST_CASE("sensor gpio server test")
     zactor_t* server = zactor_new(mlm_server, const_cast<char*>("Malamute"));
     zstr_sendx(server, "BIND", endpoint, nullptr);
 
-    printf("start server\n");
     zactor_t* self = zactor_new(fty_sensor_gpio_server, const_cast<char*>(FTY_SENSOR_GPIO_AGENT));
     REQUIRE(self);
-    printf("started server\n");
 
     // Forge a HW_CAP reply message
     // msg-correlation-id'/OK/'type'/'count'/'base_address'/'offset'/'mapping1'/'mapping_val1'/'mapping2'/'mapping_val2'/
